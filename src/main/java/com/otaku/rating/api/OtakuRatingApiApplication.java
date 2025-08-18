217a15e90ef396f097d644c92263465852f4787a
Victor Vianna <vitorvianna27@gmail.com>
2025-08-18 20:14:09 -0300

    t:tada:featÃ: First Commit

---
+++ src/main/java/com/otaku/rating/api/OtakuRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.otaku.rating.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtakuRatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtakuRatingApiApplication.class, args);
+	}
+
+}
