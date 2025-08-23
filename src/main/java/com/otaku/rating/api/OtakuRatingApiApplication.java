005327436970ccbd621f0b2295a98f7040292fcf
Leonardo Teixeira <littledoge02@gmail.com>
2025-08-22 21:24:09 -0300

    api: Fix OtakuRatingApiApplication

--- src/main/java/com/otaku/rating/api/OtakuRatingApiApplication.java
+++ src/main/java/com/otaku/rating/api/OtakuRatingApiApplication.java
@@ -3,11 +3,9 @@ package com.otaku.rating.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.otaku.rating")
 public class OtakuRatingApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OtakuRatingApiApplication.class, args);
 	}
-
 }
